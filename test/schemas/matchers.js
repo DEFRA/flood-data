d8e871640fc1272041b31d423c74a0e662524fa9
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2020-09-28 17:40:41 +0100

    Remove un-needed variable assignment

--- test/schemas/matchers.js
+++ test/schemas/matchers.js
@@ -3,18 +3,15 @@ const rloiValueParentSchema = require('./rloi-value-parent')
 const rloiValuesSchema = require('./rloi-values')
 
 const valueParentSchemaQueryMatcher = sinon.match((matchValue) => {
-  const { error } = rloiValueParentSchema.query.validate(matchValue)
-  return error === undefined
+  return rloiValueParentSchema.query.validate(matchValue).error === undefined
 }, 'parent query does not match expected schema')
 
 const valueParentSchemaVarsMatcher = sinon.match((matchValue) => {
-  const { error } = rloiValueParentSchema.vars.validate(matchValue)
-  return error === undefined
+  return rloiValueParentSchema.vars.validate(matchValue).error === undefined
 }, 'parent vars does not match expected schema')
 
 const valuesSchemaQueryMatcher = sinon.match((matchValue) => {
-  const { error } = rloiValuesSchema.query.validate(matchValue)
-  return error === undefined
+  return rloiValuesSchema.query.validate(matchValue).error === undefined
 }, 'Values query does not match expected schema')
 
 module.exports = {
