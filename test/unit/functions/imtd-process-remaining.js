c51024cb82beee12fdd2657c1d7e46af42877aed
nikiwycherley <niki.wycherley@environment-agency.gov.uk>
2023-05-12 13:04:22 +0100

    unit-tests now working

--- test/unit/functions/imtd-process-remaining.js
+++ test/unit/functions/imtd-process-remaining.js
@@ -1,103 +1,106 @@
-// 'use strict'
+'use strict'
 
-// const Lab = require('@hapi/lab')
-// const lab = (exports.lab = Lab.script())
-// const handler = require('../../../lib/functions/imtd-process').handler
-// const event = require('../../events/imtd-event-remaining.json')
-// const maxStations = require('../../data/imtd-stations').maxStations
-// const lessStations = require('../../data/imtd-stations').lessStations
-// const apiResponse = require('../../data/imtd-stations').apiResponse
-// const axios = require('axios')
+const Lab = require('@hapi/lab')
+const lab = (exports.lab = Lab.script())
+const handler = require('../../../lib/functions/imtd-process').handler
+const event = require('../../events/imtd-event-remaining.json')
+const maxStations = require('../../data/imtd-stations').maxStations
+const lessStations = require('../../data/imtd-stations').lessStations
+const apiResponse = require('../../data/imtd-stations').apiResponse
+const axios = require('axios')
+const assert = require('assert')
 
-// const s3 = require('../../../lib/helpers/s3')
-// const { Pool } = require('pg')
+const s3 = require('../../../lib/helpers/s3')
+const { Pool } = require('pg')
 
-// // start up Sinon sandbox
-// const sinon = require('sinon').createSandbox()
+// start up Sinon sandbox
+const sinon = require('sinon').createSandbox()
 
-// lab.experiment('imtd processing', () => {
-//   lab.beforeEach(async () => {
-//     process.env.LFW_DATA_DB_CONNECTION = ''
-//     // setup mocks
-//     sinon.stub(s3, 'deleteObject').callsFake(() => {
-//       return Promise.resolve({})
-//     })
-//     sinon.stub(s3, 'putObject').callsFake(() => {
-//       return Promise.resolve({})
-//     })
-//     sinon.stub(Pool.prototype, 'connect').callsFake(() => {
-//       return Promise.resolve({
-//         query: sinon.stub().resolves({}),
-//         release: sinon.stub().resolves({})
-//       })
-//     })
-//     sinon.stub(Pool.prototype, 'query').callsFake(() => {
-//       return Promise.resolve({})
-//     })
-//     sinon.stub(Pool.prototype, 'end').callsFake(() => {
-//       return Promise.resolve({})
-//     })
-//   })
-//   lab.afterEach(() => {
-//     sinon.restore()
-//   })
+lab.experiment('imtd processing', () => {
+  lab.beforeEach(async () => {
+    process.env.LFW_DATA_DB_CONNECTION = ''
+    // setup mocks
+    sinon.stub(s3, 'deleteObject').callsFake(() => {
+      return Promise.resolve({})
+    })
+    sinon.stub(s3, 'putObject').callsFake(() => {
+      return Promise.resolve({})
+    })
+    sinon.stub(Pool.prototype, 'connect').callsFake(() => {
+      return Promise.resolve({
+        query: sinon.stub().resolves({}),
+        release: sinon.stub().resolves({})
+      })
+    })
+    sinon.stub(Pool.prototype, 'query').callsFake(() => {
+      return Promise.resolve({})
+    })
+    sinon.stub(Pool.prototype, 'end').callsFake(() => {
+      return Promise.resolve({})
+    })
+  })
+  lab.afterEach(() => {
+    sinon.restore()
+  })
 
-//   lab.test('imtd process latest.json stations length over 50', async () => {
-//     const stub = sinon.stub(s3, 'getObject')
+  lab.test('imtd process latest.json stations length over 50', async () => {
+    const stub = sinon.stub(s3, 'getObject')
 
-//     stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
+    stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
 
-//     stub.onSecondCall().resolves({ Body: maxStations })
+    stub.onSecondCall().resolves({ Body: maxStations })
 
-//     function createCustomError (message, code) {
-//       const customError = new Error(message)
-//       customError.code = code
-//       return customError
-//     }
+    function createCustomError (message, code) {
+      const customError = new Error(message)
+      customError.code = code
+      return customError
+    }
 
-//     sinon.stub(axios, 'get').callsFake(() => {
-//       return Promise.resolve(apiResponse)
-//     })
+    sinon.stub(axios, 'get').callsFake(() => {
+      return Promise.resolve(apiResponse)
+    })
 
-//     await handler(event)
-//   })
+    await handler(event)
+  })
 
-//   lab.test('imtd process latest.json stations length under 50', async () => {
-//     const stub = sinon.stub(s3, 'getObject')
+  lab.test('imtd process latest.json stations length under 50', async () => {
+    const stub = sinon.stub(s3, 'getObject')
 
-//     stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
+    stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
 
-//     stub.onSecondCall().resolves({ Body: lessStations })
+    stub.onSecondCall().resolves({ Body: lessStations })
 
-//     function createCustomError (message, code) {
-//       const customError = new Error(message)
-//       customError.code = code
-//       return customError
-//     }
+    function createCustomError (message, code) {
+      const customError = new Error(message)
+      customError.code = code
+      return customError
+    }
 
-//     sinon.stub(axios, 'get').callsFake(() => {
-//       return Promise.resolve(apiResponse)
-//     })
+    sinon.stub(axios, 'get').callsFake(() => {
+      return Promise.resolve(apiResponse)
+    })
 
-//     await handler(event)
-//   })
-//   lab.test('imtd process no files found in the s3', async () => {
-//     const stub = sinon.stub(s3, 'getObject')
+    await handler(event)
+  })
+  lab.test('imtd process no files found in the s3', async () => {
+    const stub = sinon.stub(s3, 'getObject')
 
-//     stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
+    stub.onFirstCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
 
-//     stub.onSecondCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
+    stub.onSecondCall().rejects(createCustomError('Test Error', 'NoSuchKey'))
 
-//     function createCustomError (message, code) {
-//       const customError = new Error(message)
-//       customError.code = code
-//       return customError
-//     }
+    function createCustomError (message, code) {
+      const customError = new Error(message)
+      customError.code = code
+      return customError
+    }
 
-//     sinon.stub(axios, 'get').callsFake(() => {
-//       return Promise.resolve(apiResponse)
-//     })
+    sinon.stub(axios, 'get').callsFake(() => {
+      return Promise.resolve(apiResponse)
+    })
 
-//     await handler(event)
-//   })
-// })
+    await assert.rejects(async () => {
+      await handler(event)
+    }, { message: 'None of the files were found in the S3 bucket' })
+  })
+})
