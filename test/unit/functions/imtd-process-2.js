18c41fa488dba7aedd099b8ba29f9810fce335d5
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2023-06-01 17:44:47 +0100

    Tidy up tests
    * fix broken test
    * remove unused tests
    * ensure tests don't reference imtd-process 1

--- test/unit/functions/imtd-process-2.js
+++ test/unit/functions/imtd-process-2.js
@@ -9,15 +9,14 @@ const apiResponse = require('../../data/imtd-stations').apiResponse
 const axios = require('axios')
 const proxyquire = require('proxyquire')
 
-const { handler } = require('../../../lib/functions/imtd-process')
-const { handler: handler2 } = require('../../../lib/functions/imtd-process-2')
+const { handler } = require('../../../lib/functions/imtd-process-2')
 
 const { Pool } = require('pg')
 
 // start up Sinon sandbox
 const sinon = require('sinon').createSandbox()
 
-lab.experiment('imtd processing', () => {
+lab.experiment('imtd processing 2', () => {
   let queryStub
   lab.beforeEach(async () => {
     process.env.LFW_DATA_DB_CONNECTION = ''
@@ -46,31 +45,7 @@ lab.experiment('imtd processing', () => {
     await handler(event)
   })
 
-  lab.test('test test', async () => {
-    const promises = [1, 2, 3, 4, 5].map(async stationId => {
-      return stationId
-    })
-
-    const test = await Promise.all(promises)
-
-    Code.expect(test[0]).to.equal(1)
-  })
-
-  lab.test('imtd process should handle response with thresholds thresholds', async () => {
-    const apiResponseX = {
-      status: 200,
-      statusText: 'OK',
-      data: [
-        {
-          RLOIid: '1165',
-          wiskiID: '254290001',
-          telemetryID: 'E10023',
-          Name: 'Tanbridge GS',
-          TimeSeriesMetaData: []
-        }
-      ]
-    }
-
+  lab.test('imtd process should handle response with thresholds', async () => {
     const axiosStub = sinon.stub(axios, 'get').callsFake(() => {
       return Promise.resolve(apiResponse)
     })
@@ -80,24 +55,24 @@ lab.experiment('imtd processing', () => {
       error: sinon.spy()
     }
 
-    const { handler } = proxyquire('../../../lib/functions/imtd-process', {
+    const { handler } = proxyquire('../../../lib/functions/imtd-process-2', {
       '../helpers/logging': log
     })
 
     await handler(event)
-    // Code.expect(axiosStub.callCount).to.equal(8)
-    // Code.expect(log.error.callCount).to.equal(0)
+    Code.expect(axiosStub.callCount).to.equal(8)
+    Code.expect(log.error.callCount).to.equal(0)
     // 8 stations each with the same 6 thresholds (out of 10 thresholds for inclusion)
     /// 48 inserts + 1 select and 8 drops = 57
     Code.expect(queryStub.callCount).to.equal(57)
   })
 
-  lab.test.only('imtd process should handle response with thresholds', async () => {
+  lab.test('imtd process should handle response with thresholds', async () => {
     const axiosStub = sinon.stub(axios, 'get').callsFake(() => {
       return Promise.resolve(apiResponse)
     })
 
-    await handler2(event)
+    await handler(event)
     // 8 stations in db response stub
     Code.expect(axiosStub.callCount).to.equal(8)
     // 8 stations each with the same 6 thresholds (out of 10 thresholds for inclusion)
