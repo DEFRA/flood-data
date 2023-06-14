545df75a93f7a803e0c1a0ee106b2d76c9be3b2b
personal <personal@personals-MacBook-Pro.local>
2023-06-14 10:11:57 +0100

    add more unit tests to lambda functions with better test coverage  Committer: personal <personal@personals-MacBook-Pro.local>

--- test/unit/functions/rloi-process.js
+++ test/unit/functions/rloi-process.js
@@ -1,50 +1,64 @@
 const Lab = require('@hapi/lab')
-const lab = exports.lab = Lab.script()
-const Code = require('@hapi/code')
+const { it, describe, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = require('@hapi/code')
 const handler = require('../../../lib/functions/rloi-process').handler
 const event = require('../../events/fwis-event.json')
 const s3 = require('../../../lib/helpers/s3')
 const util = require('../../../lib/helpers/util')
 const rloi = require('../../../lib/models/rloi')
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 // start up Sinon sandbox
-const sinon = require('sinon').createSandbox()
+const sinon = require('sinon')
 
-lab.experiment('rloi processing', () => {
-  lab.beforeEach(() => {
-    // setup mocks
-    sinon.stub(s3, 'getObject').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(util, 'parseXml').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(rloi, 'save').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'connect').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'query').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'end').callsFake(() => {
-      return Promise.resolve({})
-    })
-  })
-  lab.afterEach(() => {
-    // restore sinon sandbox
-    sinon.restore()
-  })
-  lab.test('rloi process', async () => {
-    await handler(event)
+describe('handler', () => {
+  let sandbox
+  let s3Stub
+  let poolStub
+  let parseXmlStub
+  let saveStub
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+    s3Stub = sandbox.stub(s3, 'getObject')
+    poolStub = sandbox.stub(Pool.prototype, 'end')
+    parseXmlStub = sandbox.stub(util, 'parseXml')
+    saveStub = sandbox.stub(rloi, 'save')
+    // Initialize your event here
   })
 
-  lab.test('rloi process S3 error', async () => {
-    s3.getObject = () => {
-      return Promise.reject(new Error('test error'))
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should handle event and return result', async () => {
+    s3Stub.resolves({ Body: 'test body' })
+    parseXmlStub.resolves('parsed xml')
+    saveStub.resolves('save response')
+    poolStub.resolves()
+
+    const result = await handler(event)
+
+    sinon.assert.calledOnce(s3Stub)
+    sinon.assert.calledOnce(parseXmlStub)
+    sinon.assert.calledOnce(saveStub)
+    sinon.assert.calledOnce(poolStub)
+
+    expect(result).to.equal('save response')
+  })
+
+  it('should handle errors', async () => {
+    const error = new Error('test error')
+    s3Stub.rejects(error)
+
+    try {
+      await handler(event)
+    } catch (err) {
+      sinon.assert.calledOnce(s3Stub)
+      sinon.assert.notCalled(parseXmlStub)
+      sinon.assert.notCalled(saveStub)
+      sinon.assert.notCalled(poolStub)
+      expect(err).to.equal(error)
     }
-    await Code.expect(handler(event)).to.reject()
   })
 })
--- test/unit/functions/station-process.js
+++ test/unit/functions/station-process.js
@@ -1,6 +1,6 @@
 const Lab = require('@hapi/lab')
-const lab = exports.lab = Lab.script()
-const Code = require('@hapi/code')
+const { it, describe, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = require('@hapi/code')
 const handler = require('../../../lib/functions/station-process').handler
 const event = require('../../events/station-event.json')
 const s3 = require('../../../lib/helpers/s3')
@@ -9,49 +9,71 @@ const station = require('../../../lib/models/station')
 const { Client } = require('pg')
 
 // start up Sinon sandbox
-const sinon = require('sinon').createSandbox()
+const sinon = require('sinon')
 
-lab.experiment('station processing', () => {
-  lab.beforeEach(() => {
-    // setup mocks
-    sinon.stub(s3, 'getObject').callsFake(() => {
-      return new Promise((resolve, reject) => {
-        resolve({
-          Body: 'test'
-        })
-      })
-    })
-    sinon.stub(util, 'parseCsv').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(station, 'saveToDb').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(station, 'saveToObjects').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'connect').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'query').callsFake(() => {
-      return Promise.resolve({})
-    })
-    sinon.stub(Client.prototype, 'end').callsFake(() => {
-      return Promise.resolve({})
-    })
-  })
-  lab.afterEach(() => {
-    // restore sinon sandbox
-    sinon.restore()
-  })
-  lab.test('station process', async () => {
-    await handler(event)
+describe('handler', () => {
+  let sandbox
+  let s3Stub
+  let clientStub
+  let clientEndStub
+  let parseCsvStub
+  let saveToDbStub
+  let saveToObjectsStub
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+    s3Stub = sandbox.stub(s3, 'getObject')
+    clientStub = sandbox.stub(Client.prototype, 'connect')
+    clientEndStub = sandbox.stub(Client.prototype, 'end')
+    parseCsvStub = sandbox.stub(util, 'parseCsv')
+    saveToDbStub = sandbox.stub(station, 'saveToDb')
+    saveToObjectsStub = sandbox.stub(station, 'saveToObjects')
+    // Initialize your event here
   })
 
-  lab.test('station process S3 error', async () => {
-    s3.getObject = () => {
-      return Promise.reject(new Error('test error'))
+  afterEach(() => {
+    sandbox.restore()
+    s3Stub.restore()
+    parseCsvStub.restore()
+    clientStub.restore()
+    clientEndStub.restore()
+    saveToDbStub.restore()
+    saveToObjectsStub.restore()
+  })
+
+  it('should handle event and return result', async () => {
+    s3Stub.resolves({ Body: 'test body' })
+    parseCsvStub.resolves('parsed csv')
+    clientStub.resolves()
+    clientEndStub.resolves()
+    saveToDbStub.resolves('save to db response')
+    saveToObjectsStub.resolves('save to s3 response')
+
+    const result = await handler(event)
+
+    sinon.assert.calledOnce(s3Stub)
+    sinon.assert.calledOnce(parseCsvStub)
+    sinon.assert.calledOnce(clientStub)
+    sinon.assert.calledOnce(clientEndStub)
+    sinon.assert.calledOnce(saveToDbStub)
+    sinon.assert.calledOnce(saveToObjectsStub)
+    expect(result).to.equal({ saveStationsToDb: 'save to db response', saveStationsToS3: 'save to s3 response' })
+  })
+
+  it('station process handler should handle errors', { timeout: 15000 }, async () => {
+    const error = new Error('test error')
+    s3Stub.rejects(error)
+
+    try {
+      await handler(event)
+    } catch (err) {
+      sinon.assert.calledOnce(s3Stub)
+      sinon.assert.notCalled(parseCsvStub)
+      sinon.assert.notCalled(clientStub)
+      sinon.assert.notCalled(clientEndStub)
+      sinon.assert.notCalled(saveToDbStub)
+      sinon.assert.notCalled(saveToObjectsStub)
+      expect(err).to.equal(error)
     }
-    await Code.expect(handler(event)).to.reject()
   })
 })
