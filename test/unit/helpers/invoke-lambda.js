cad3156888b6b705f91efa5d4b8ee3c08c4c70e0
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-07-10 17:58:06 +0100

    Migrate to AWS SDK v3

--- test/unit/helpers/invoke-lambda.js
+++ test/unit/helpers/invoke-lambda.js
@@ -1,31 +1,64 @@
 const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
 const Code = require('@hapi/code')
-const proxyquire = require('proxyquire')
-const sinon = require('sinon')
+const { mockClient } = require('aws-sdk-client-mock')
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
 
-const mocks = {
-  invokeAsync: sinon.stub()
-}
+// Import the module under test
+const invokeLambda = require('../../../lib/helpers/invoke-lambda')
 
-const invokeLambda = proxyquire('../../../lib/helpers/invoke-lambda', {
-  'aws-sdk': {
-    Lambda: function Lambda () {
-      this.invokeAsync = mocks.invokeAsync
-    }
-  }
-})
+// Create the mock
+const lambdaMock = mockClient(LambdaClient)
 
 lab.experiment('invokeLambda', () => {
-  lab.test('it invokesAsync the function passing the payload as its InvokeArgs', () => {
-    mocks.invokeAsync.returns({ promise: async () => {} })
+  lab.beforeEach(() => {
+    lambdaMock.reset()
+  })
 
-    invokeLambda('some-function', { foo: 'bar' })
+  lab.test('it invokes the function passing the payload as its InvokeArgs', async () => {
+    lambdaMock.on(InvokeCommand).resolves({ Payload: JSON.stringify({}) })
+    await invokeLambda('some-function', { foo: 'bar' })
 
-    Code.expect(mocks.invokeAsync.getCalls().length).to.equal(1)
-    Code.expect(mocks.invokeAsync.getCalls()[0].args).to.equal([{
+    const calls = lambdaMock.commandCalls(InvokeCommand)
+    Code.expect(calls.length).to.equal(1)
+    Code.expect(calls[0].args[0].input).to.equal({
       FunctionName: 'some-function',
-      InvokeArgs: '{"foo":"bar"}'
-    }])
+      Payload: Buffer.from(JSON.stringify({ foo: 'bar' }))
+    })
+  })
+
+  lab.test('it should throw an error when the Lambda invocation response payload contains a FunctionError with an error message and stack trace', async () => {
+    const errorMessage = 'Lambda error'
+    const stackTrace = ['mock trace']
+    const mockResponse = {
+      FunctionError: 'Unhandled',
+      Payload: JSON.stringify({ errorMessage, stackTrace })
+    }
+
+    lambdaMock.on(InvokeCommand).resolves(mockResponse)
+
+    const err = await Code.expect(invokeLambda('some-function', { foo: 'bar' })).to.reject()
+    const invokeCalls = lambdaMock.commandCalls(InvokeCommand)
+    Code.expect(invokeCalls).to.have.length(1)
+    Code.expect(lambdaMock.send.calledOnce).to.be.true()
+    Code.expect(err.message).to.equal(errorMessage)
+    Code.expect(err.stack).to.equal(stackTrace)
+  })
+
+  lab.test('it should throw an error when the Lambda invocation response payload contains a FunctionError without an error message and stack trace', async () => {
+    const errorMessage = 'Lambda invocation failed'
+    const mockResponse = {
+      FunctionError: 'Unhandled',
+      Payload: JSON.stringify({})
+    }
+
+    lambdaMock.on(InvokeCommand).resolves(mockResponse)
+
+    const err = await Code.expect(invokeLambda('some-function', { foo: 'bar' })).to.reject()
+    const invokeCalls = lambdaMock.commandCalls(InvokeCommand)
+    Code.expect(invokeCalls).to.have.length(1)
+    Code.expect(lambdaMock.send.calledOnce).to.be.true()
+    Code.expect(err.message).to.equal(errorMessage)
+    Code.expect(err.stack).to.contain(errorMessage)
   })
 })
---
+++ test/unit/helpers/s3.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const lab = exports.lab = Lab.script()
+const { mockClient } = require('aws-sdk-client-mock')
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  ListObjectsV2Command
+} = require('@aws-sdk/client-s3')
+
+const s3Wrapper = require('../../../lib/helpers/s3')
+
+const s3Mock = mockClient(S3Client)
+
+lab.experiment('S3 Wrapper Module', () => {
+  lab.beforeEach(() => {
+    s3Mock.reset()
+  })
+
+  lab.test('getObject should send GetObjectCommand with correct params', async () => {
+    const params = { Bucket: 'test-bucket', Key: 'test-key' }
+    s3Mock.on(GetObjectCommand).resolves({ Body: 'mocked-body' })
+
+    const result = await s3Wrapper.getObject(params)
+
+    Code.expect(result.Body).to.equal('mocked-body')
+    Code.expect(s3Mock.commandCalls(GetObjectCommand, params).length).to.equal(1)
+  })
+
+  lab.test('putObject should send PutObjectCommand with ACL set', async () => {
+    const params = { Bucket: 'test-bucket', Key: 'test-key', Body: 'data' }
+    s3Mock.on(PutObjectCommand).resolves({ ETag: '"etag"' })
+
+    const result = await s3Wrapper.putObject({ ...params })
+
+    Code.expect(result.ETag).to.equal('"etag"')
+    const calls = s3Mock.commandCalls(PutObjectCommand)
+    Code.expect(calls.length).to.equal(1)
+    Code.expect(calls[0].args[0].input.ACL).to.equal('bucket-owner-full-control')
+  })
+
+  lab.test('deleteObject should send DeleteObjectCommand with correct params', async () => {
+    const params = { Bucket: 'test-bucket', Key: 'test-key' }
+    s3Mock.on(DeleteObjectCommand).resolves({})
+
+    const result = await s3Wrapper.deleteObject(params)
+
+    Code.expect(result).to.be.an.object()
+    Code.expect(Object.keys(result)).to.have.length(0)
+    Code.expect(s3Mock.commandCalls(DeleteObjectCommand, params).length).to.equal(1)
+  })
+
+  lab.test('listObjects should send ListObjectsV2Command with correct params', async () => {
+    const params = { Bucket: 'test-bucket' }
+    s3Mock.on(ListObjectsV2Command).resolves({ Contents: [] })
+
+    const result = await s3Wrapper.listObjects(params)
+
+    Code.expect(result.Contents).to.be.an.array()
+    Code.expect(s3Mock.commandCalls(ListObjectsV2Command, params).length).to.equal(1)
+  })
+})
