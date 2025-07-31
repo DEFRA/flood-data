52b18481d900ad86c804e812c5e3c8e5002706ef
Paul Wadmore <8915039+pwadmore-ea@users.noreply.github.com>
2025-07-31 13:09:50 +0100

    feature/FSR-1136-update-to-aws-sdk-v3 (#104)
    * feature/FSR-1136-update-to-aws-sdk-v3
    
    https://eaflood.atlassian.net/browse/FSR-1136
    
    * Migrate to AWS SDK v3
    
    * Update dependencies
    
    * Use transformToString calls
    
    * Use asynchronous Lambda invocation
    
    * Remove async function declarations
    
    * Remove async function declarations
    
    * Correct case
    
    * Support S3 configuration
    
    * Consume fetched station data using transformToString
    
    * Resolve SonarQube cloud issue
    
    * Rename environment variables for consistency
    
    * Correct type of response body
    
    * Refactor DEFAULT_MAX_SOCKETS
    
    * Remove environment variable defaults

--- test/integration/rloi-process.js
+++ test/integration/rloi-process.js
@@ -1,19 +1,31 @@
 'use strict'
 const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
-const AWS = require('aws-sdk')
-AWS.config.update({ region: process.env.LFW_DATA_TARGET_REGION })
-const lambda = new AWS.Lambda()
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
+
+const lambdaClient = new LambdaClient({
+  region: process.env.LFW_DATA_TARGET_REGION
+})
 
 lab.experiment('Test rloiProcess lambda invoke', () => {
   lab.test('rloiProcess invoke no event expect error', async () => {
-    const data = await lambda.invoke({ FunctionName: `${process.env.LFW_DATA_TARGET_ENV_NAME}${process.env.LFW_DATA_SERVICE_CODE}-rloiProcess` }).promise()
+    const command = new InvokeCommand({
+      FunctionName: `${process.env.LFW_DATA_TARGET_ENV_NAME}${process.env.LFW_DATA_SERVICE_CODE}-rloiProcess`
+    })
+
+    const data = await lambdaClient.send(command)
+
     if (data.StatusCode !== 200) {
       throw new Error('rloiProcess non 200 response')
     }
-    const payload = JSON.parse(data.Payload)
-    if (!payload || !payload.errorMessage) {
+
+    if (!data.FunctionError) {
       throw new Error('rloiProcess should have errored')
+    }
+
+    const payload = JSON.parse(Buffer.from(data.Payload).toString())
+    if (!payload?.errorMessage) {
+      throw new Error('rloiProcess errorMessage missing in payload')
     }
   })
 })
--- test/unit/helpers/invoke-lambda.js
+++ test/unit/helpers/invoke-lambda.js
@@ -1,31 +1,42 @@
 const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
 const Code = require('@hapi/code')
-const proxyquire = require('proxyquire')
-const sinon = require('sinon')
+const { mockClient } = require('aws-sdk-client-mock')
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
 
-const mocks = {
-  invokeAsync: sinon.stub()
-}
+// Import the module under test
+const invokeLambda = require('../../../lib/helpers/invoke-lambda')
 
-const invokeLambda = proxyquire('../../../lib/helpers/invoke-lambda', {
-  'aws-sdk': {
-    Lambda: function Lambda () {
-      this.invokeAsync = mocks.invokeAsync
-    }
-  }
-})
+// Create the mock
+const lambdaMock = mockClient(LambdaClient)
 
 lab.experiment('invokeLambda', () => {
-  lab.test('it invokesAsync the function passing the payload as its InvokeArgs', () => {
-    mocks.invokeAsync.returns({ promise: async () => {} })
+  lab.beforeEach(() => {
+    lambdaMock.reset()
+  })
 
-    invokeLambda('some-function', { foo: 'bar' })
+  lab.test('it invokes the function passing the payload as its InvokeArgs', async () => {
+    lambdaMock.on(InvokeCommand).resolves({ Payload: JSON.stringify({}) })
+    await invokeLambda('some-function', { foo: 'bar' })
 
-    Code.expect(mocks.invokeAsync.getCalls().length).to.equal(1)
-    Code.expect(mocks.invokeAsync.getCalls()[0].args).to.equal([{
+    const calls = lambdaMock.commandCalls(InvokeCommand)
+    Code.expect(calls.length).to.equal(1)
+    Code.expect(calls[0].args[0].input).to.equal({
       FunctionName: 'some-function',
-      InvokeArgs: '{"foo":"bar"}'
-    }])
+      Payload: Buffer.from(JSON.stringify({ foo: 'bar' })),
+      InvocationType: 'Event'
+    })
+  })
+
+  lab.test('it should throw an error when the Lambda invocation fails', async () => {
+    const mockErrorMessage = 'mock error'
+    const mockError = new Error(mockErrorMessage)
+    lambdaMock.on(InvokeCommand).rejects(mockError)
+
+    const err = await Code.expect(invokeLambda('some-function', { foo: 'bar' })).to.reject()
+    const invokeCalls = lambdaMock.commandCalls(InvokeCommand)
+    Code.expect(invokeCalls).to.have.length(1)
+    Code.expect(lambdaMock.send.calledOnce).to.be.true()
+    Code.expect(err.message).to.equal(mockErrorMessage)
   })
 })
